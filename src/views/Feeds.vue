6868d508012ad2fe56c139629ad96b04e50f91d6
Agbama Akem <ulimhunyieagbama@gmail.com>
2023-03-22 04:44:42 +0100

    Fix chats ui issues

--- src/views/Feeds.vue
+++ src/views/Feeds.vue
@@ -287,9 +287,9 @@
                                 <span v-if="feed.for === 'sale'">Forever</span>
                             </p>
                             <svg xmlns="http://www.w3.org/2000/svg" v-motion :initial="{ opacity: 0.8 }"
-                                :tapped="{ opacity: 1, y: 0, x: 0, scale: 1.2 }" fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cursor-pointer"
-                                @click="saveAd(feed._id)"
+                                v-if="$store.state.isAuthenticated" :tapped="{ opacity: 1, y: 0, x: 0, scale: 1.2 }"
+                                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                class="w-6 h-6 cursor-pointer" @click="saveAd(feed._id)"
                                 :class="{ 'text-orange-400': $store.state.user.savedAds.includes(feed._id) }">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
