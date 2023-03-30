7228f798428b9e5602c713a2d3ca97eabfe20bde
Agbama Jnr <ulimhunyieagbama@gmail.com>
2023-03-30 13:39:51 +0100

    Update ws url

--- src/views/chats/components/Chat.vue
+++ src/views/chats/components/Chat.vue
@@ -93,7 +93,7 @@ function sortedChats() {
 
 }
 
-const socket = io("http://localhost:2023", {
+const socket = io("https://habeep.org", {
     path: '/backend',
     auth: {
         token: store.state.sessionId
