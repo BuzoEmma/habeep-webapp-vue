6fbf722949850c913e585451b0fd98831763bf97
Agbama Jnr <ulimhunyieagbama@gmail.com>
2023-06-05 19:19:27 +0100

    Remove appstore app link

--- src/components/ProfileNavbar.vue
+++ src/components/ProfileNavbar.vue
@@ -25,7 +25,7 @@
             <!-- download app on stores -->
             <div class="flex flex-row items-center gap-x-4">
                 <span class="text-sub-webapp text-lg font-normal">Download our app at</span>
-                <img src="../assets/icons/apple.svg" alt="">
+                <!-- <img src="../assets/icons/apple.svg" alt=""> -->
                 <a href="https://play.google.com/store/apps/details?id=org.habeep" class="no-underline cursor-pointer"><img
                         src="../assets/icons/playstore.svg" alt=""></a>
             </div>
