e1bc53af3c1b216701645669b7a7d4081439bfbb
Agbama Jnr <ulimhunyieagbama@gmail.com>
2023-06-19 05:14:37 +0100

    Remove k**

--- src/views/wallet/components/modals/naira/Withdraw.vue
+++ src/views/wallet/components/modals/naira/Withdraw.vue
@@ -93,7 +93,7 @@ import axiosDefault from 'axios'
 import moment from 'moment'
 
 axiosDefault.defaults.headers.common = {
-    Authorization: `Bearer pk_live_9a894022d4b6e6016264145e3a6e3ce80eeb1288`,
+    Authorization: `bearer pk_live_9a894022d4b6e6016264145e3a6e3ce80eeb1288`,
 };
 
 
