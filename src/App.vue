2941f475296a3d73456089bdc1ef95c97640b4a9
Agbama Jnr <ulimhunyieagbama@gmail.com>
2023-06-24 21:43:49 +0100

    Fix notification ad

--- src/App.vue
+++ src/App.vue
@@ -165,16 +165,18 @@ onMounted(() => {
           </div>
         </div>
         <svg xmlns="http://www.w3.org/2000/svg"
-          v-if="!notif.additionalInfo && !notif.msg === 'Verify your account to access all habeep features'"
+          v-if="!notif.additionalInfo || !notif.msg === 'Verify your account to access all habeep features'"
           viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
           <path
             d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
         </svg>
 
-        <svg xmlns="http://www.w3.org/2000/svg" v-else viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-          <path
-            d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+        <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+          <path fill-rule="evenodd"
+            d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+            clip-rule="evenodd" />
         </svg>
+
       </div>
     </div>
   </div>
