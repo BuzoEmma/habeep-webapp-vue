5d56f1252d450b4b2c4347411940ff51ab1c09e6
Agbama Jnr <ulimhunyieagbama@gmail.com>
2023-06-28 19:53:26 +0100

    Edit profile

--- src/views/profile/agents/Profile.vue
+++ src/views/profile/agents/Profile.vue
@@ -267,7 +267,7 @@ const router = useRouter()
 const store = useStore()
 
 const title = ref('Habeep | ' + route.params.username + ' Profile')
-const content = ref('This is ' + route.params.username + ' Profile')
+const content = ref('View more information about @' + route.params.username)
 const img = ref('https://i.ibb.co/DtbR0BW/user.png')
 import { useHead } from '@vueuse/head'
 
