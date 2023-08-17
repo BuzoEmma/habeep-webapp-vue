a253a44347df0467dc8a6bc526350b2669537192
Agbama Jnr <ulimhunyieagbama@gmail.com>
2023-08-17 16:03:49 +0100

    Improve seo on homepage

--- src/App.vue
+++ src/App.vue
@@ -151,7 +151,7 @@ onMounted(() => {
 
 
     <div
-      class="flex-col flex absolute bottom-4 md:bottom-10 h-fit z-50 items-center w-full sm:w-fit justify-end p-2 gap-y-1 right-2 md:right-12"
+      class="flex-col flex fixed bottom-4 md:bottom-10 h-fit z-50 items-center w-full sm:w-fit justify-end p-2 gap-y-1 right-2 md:right-12"
       v-if="allNotifications.length > 0 && $store.state.isAuthenticated">
       <div class="flex-row-center w-full p-4 notif justify-between cursor-pointer" @click="takeNotificationAction(notif)"
         v-motion :initial="{ y: -100 }" :enter="{ y: 0 }" :tapped="{ opacity: 0, transition: { delay: 100 } }"
