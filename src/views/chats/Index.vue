f10093c178ab5990c9b9b2d9265b5ae56667a0cf
Agbamajnr <ulimhunyieagbama@gmail.com>
2024-03-19 14:12:17 +0100

    make chat sync

--- src/views/chats/Index.vue
+++ src/views/chats/Index.vue
@@ -98,12 +98,12 @@ async function getRooms() {
                     enterChatBox(room)
                     setTimeout(() => {
                         leaveChat()
-                    }, 200);
-                }, 300);
+                    }, 1000);
+                }, 2000);
                 setTimeout(() => {
                     clearInterval(roomInterval)
                     callback()
-                }, allRooms.value.length * 500);
+                }, allRooms.value.length * 1100);
             }, function () {
                 leaveChat()
                 processing.value = false
