08825cfcfbd5bc682b69dbc5360822fdfa119ff8
BuzoEmma <chibuzoemmanuel55@gmail.com>
2024-08-27 17:23:43 +0100

    Made a route change on the AffiliateFee Page

--- src/views/profile/IBO/register/AffiliateFee.vue
+++ src/views/profile/IBO/register/AffiliateFee.vue
@@ -263,6 +263,7 @@ async function makeUserAnIBO() {
                         router.replace('/tenancy')
                     }
                 }
+                
             }, 2000);
             
         } else {
