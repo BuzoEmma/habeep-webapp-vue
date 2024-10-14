5296476312b08bc1c531153b3600b2aedfbe621b
BuzoEmma <chibuzoemmanuel55@gmail.com>
2024-10-14 18:09:30 +0100

    Updated testimonial content

--- src/views/about/aboutTestimonials.js
+++ src/views/about/aboutTestimonials.js
@@ -1,32 +1,16 @@
-import img1 from "../../assets/images/testimonial.png";
-import img2 from "../../assets/images/testimonial.png";
-import img3 from "../../assets/images/testimonial.png";
-import img4 from "../../assets/images/testimonial.png";
+import MrJohn from "../../assets/images/MrJohn.jpg";
 
 const testimonials = [
   {
-    Name: "Bassey Edit",
-    img: img1,
-    comment:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, accusamus",
-  },
-  {
-    Name: "Buzo Buzo",
-    img: img2,
-    comment:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, accusamus",
-  },
-  {
-    Name: "Chief Osita",
-    img: img3,
-    comment:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, accusamus",
-  },
-  {
-    Name: "Chiamaka Edit",
-    img: img4,
-    comment:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, accusamus",
+    Name: "John Johnson",
+    img: MrJohn,
+    comment: `As a tenant, using Habeep has made paying my rent incredibly 
+    and stress-free. The app offers a straightforward process for making
+    payments directly to my landlady, and I was able to set up a flexible,
+    affordable repayment plan with a minimal fee. The entire experience was 
+    smooth, and the transparency of the app gave me confidence that my payments
+    were securely processed. I highly recommend Habeep to any tenant looking 
+    for a hassle-free and affordable way to manage rent payments.`,
   },
 ];
 
