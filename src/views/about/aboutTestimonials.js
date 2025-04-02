736c2a7ce683ffbe1e06655be312b354d5ddb4a1
BuzoEmma <chibuzoemmanuel55@gmail.com>
2025-04-02 16:36:39 +0100

    testimonials

--- src/views/about/aboutTestimonials.js
+++ src/views/about/aboutTestimonials.js
@@ -8,7 +8,7 @@ const testimonials = [
     Their savings plan helped me set aside funds for my rent with ease,
     and when I needed extra support, their reliable rent loan service was
     a lifesaver. I highly recommend them to any tenant looking for financial
-    flexibility and peace of mind!`,
+    flexibility and peace of mind.`,
   },
 ];
 
